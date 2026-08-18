dd04cffd65f92ea764af4e843c1cdee37c1bcada
Vashu Tomar <vashutomar2003@gmail.com>
2026-08-18 19:27:28 +0530

    JS VARIABLES & DATATYPES

--- JavaScript/02_VariablesDataTypes.js
+++ JavaScript/02_VariablesDataTypes.js
@@ -24,29 +24,37 @@ let a =10;
 console.log(a);
 
 // Outside the block it will throw an error :: //
-{
-    let b =10;
-}
-console.log(b);
+// {
+//     let b =10;
+// }
+// console.log(b);
 
 // redefine or redeclare must not be allowed :: 
 // it will throw an error
 
-let a = 5;
-let a = 10;
+// let a = 5;
+// let a = 10;
 
 
 // dynamically-typed variable :: 
 // we can assign multiple-types of value to store in it ::
 
 let name = "bacd";
-let name = 10;
+// let name = 10;
 
 // CONST KEYWORD ::
 // it's value will not be changed but declared permanently unlike let keyword ::
 
-const a =28;
+const c =28;
+console.log(c);
+
+// a = 50;
 console.log(a);
 
-a = 50;
-console.log(a);
+// Data Types :: 
+
+let marks = 20;
+console.log("marks");
+
+let mark;
+console.log(typeof(marks));

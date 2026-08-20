c78a839e28358ba5b1e3617e8cb2a62e4598004e
Vashu Tomar <vashutomar2003@gmail.com>
2026-08-20 10:47:36 +0530

    Basic JS

--- JavaScript/01_Basics.js
+++ JavaScript/01_Basics.js
@@ -1,1 +1,5 @@
 console.log("Hello Everyone")
+
+// It's not necessary to add semicolon at the end of the statement/logic in the javascript.
+
+// But if you are writing two or more statement/logic in the same line then you can add ; after every statement.

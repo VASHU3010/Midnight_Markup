e6754a0d21be00e96bd9c7d57dcecc4b903f2a2f
Vashu Tomar <vashutomar2003@gmail.com>
2026-08-28 06:58:25 +0530

    react basic app

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import heroImg from './assets/hero.png'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   const [count, setCount] = useState(0)
 
-  return (
-    <>
-    )
-    }
-export default App
+//   return (
+//     <>
+//     )
+//     }
+// export default App

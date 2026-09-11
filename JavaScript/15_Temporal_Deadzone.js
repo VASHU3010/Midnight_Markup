828cc530c9b97db5ba228b1993bfefcdce33a088
Vashu Tomar <vashutomar2003@gmail.com>
2026-09-11 19:38:49 +0530

    Temporal Deadzone

---
+++ JavaScript/15_Temporal_Deadzone.js
@@ -0,0 +1,11 @@
+// What is Temporal Dead zone ?
+
+// by the help of let and var keyword you created a variable ,
+// Variable declaration moved yo top only in var case. by hoisting but only possible in var . 
+
+// this is temporal dead - zone.  
+console.log(marks)
+console.log(marks)
+console.log(marks)
+const marks = 100 //predictability will be increased. 
+console.log(marks) //but it will not be accessed by the code 

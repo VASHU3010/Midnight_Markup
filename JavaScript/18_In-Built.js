ea0ca08dcdeb78f519125506cf99dae1a953baa3
Vashu Tomar <vashutomar2003@gmail.com>
2026-09-11 19:40:35 +0530

    In-Builts in JS

---
+++ JavaScript/18_In-Built.js
@@ -0,0 +1,39 @@
+// 1. MATH OBJECT ::
+
+console.log(Math.PI)
+console.log(Math.max(45,67,89,90,100,509009))
+console.log(Math.min(45,67,89,90,100,509009))
+
+console.log(Math.round(1.4))
+console.log(Math.floor(1.9))
+
+console.log(Math.ceil(1.1))
+
+console.log(Math.abs(-5))
+
+console.log(Math.random()) //between 0 and 1 
+
+console.log(Math.sqrt(5))
+
+console.log(Math.pow(2,10))
+
+console.log(Math.sin(90))
+
+// 2. DATA OBJECT ::
+
+let curr = new Date()
+new Date()
+
+
+let date = new Date("june 20, 1998")
+console.log(date)
+
+console.log(curr)
+
+let newDate =  new Date(1990, 8, 11)
+console.log(newDate)
+
+console.log(newDate.getDay())
+console.log(newDate.setFullYear(2026))
+
+console.log(newDate.getFullYear())
